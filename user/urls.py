8a429f5a11e30ee7223a24108ff0ef5cc091d80b
konstantin <kkononenko3@gmail.com>
2023-08-03 18:14:30 +0300

    create project, make project structure (add user and blog app)

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+# router.register("posts", PostViewSet)
+# router.register("authors", AuthorViewSet)
+# router.register("category`s", CategoryViewSet)
+# router.register("comments", CommentViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+app_name = "user"
