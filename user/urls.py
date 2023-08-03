ebd11fb45e1280d26a7f59c920e881f12ac501ad
konstantin <kkononenko3@gmail.com>
2023-08-03 19:22:42 +0300

    create token authentication

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
+from rest_framework.authtoken import views
 
-from user.views import CreateUserView, ListUserView
-
-router = routers.DefaultRouter()
-router.register("all_users", ListUserView)
-
+from user.views import CreateUserView, ListUserView, CreateTokenView
 
 urlpatterns = [
     path("", ListUserView.as_view(), name="user-all"),
     path("register/", CreateUserView.as_view(), name="user-register"),
+    path('token/', CreateTokenView.as_view(), name="user-token")
 ]
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.models import User
 from user.serializers import UserSerializer
@@ -11,3 +13,7 @@ class CreateUserView(generics.CreateAPIView):
 class ListUserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
