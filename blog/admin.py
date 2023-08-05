cf276c8f275c3e51228908b4cd459b52d0a71302
konstantin <kkononenko3@gmail.com>
2023-08-05 12:01:10 +0300

    create admin,register for Category, UserFollowing, Post, Like, Comment + added information to database

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,43 @@ from django.contrib import admin
 from blog.models import (Category, UserFollowing, Post, Like, Comment)
 
 admin.site.register(Category)
-admin.site.register(UserFollowing)
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Comment)
+
+
+@admin.register(UserFollowing)
+class UserFollowingAdmin(admin.ModelAdmin):
+    list_display = [
+        "user_id",
+        "following_user_id",
+        "created",
+    ]
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "author",
+        "content",
+    ]
+    search_fields = ("title",)
+    list_filter = ("title",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "content",
+        "date_posted",
+    ]
+    search_fields = ("content",)
+    list_filter = ("content",)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = [
+        "created_by",
+        "post",
+    ]
+    search_fields = ("created_by",)
+    list_filter = ("created_by",)
