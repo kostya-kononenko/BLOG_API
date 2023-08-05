1dcc3bfa5103007ab667e80a061012c9b65f512b
konstantin <kkononenko3@gmail.com>
2023-08-05 11:40:07 +0300

    create admins for Category, UserFollowing, Post, Like, Comment + added information to database

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import (Category, UserFollowing, Post, Like, Comment)
+
+admin.site.register(Category)
+admin.site.register(UserFollowing)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
